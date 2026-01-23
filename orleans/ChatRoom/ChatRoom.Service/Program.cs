f038706df4c0b74a60c2aad54815f315a9956b98
Jesse.vanEvert <Jesse.vanEvert@siebeljuweliers.nl>
2026-01-23 10:12:03 +0100

    ??

---
+++ orleans/ChatRoom/ChatRoom.Service/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+await Host.CreateDefaultBuilder(args)
+    .UseOrleans(siloBuilder => siloBuilder.UseLocalhostClustering()
+            .AddMemoryGrainStorage("PubSubStore")
+            .AddMemoryStreams("chat")
+            .ConfigureLogging(logging => logging.AddConsole()))
+    .RunConsoleAsync();
+
